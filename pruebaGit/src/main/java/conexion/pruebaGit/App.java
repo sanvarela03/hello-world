8f2786d4133f03b2548efb4a9426f4059598c30f
sanvarela03 <42015219+sanvarela03@users.noreply.github.com>
2019-05-13 00:16:18 -0500

    Versión inicial programa prueba.

---
+++ pruebaGit/src/main/java/conexion/pruebaGit/App.java
@@ -0,0 +1,13 @@
+package conexion.pruebaGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
